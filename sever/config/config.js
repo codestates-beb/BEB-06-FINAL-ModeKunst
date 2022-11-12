e8ec1297e2ed2222b256ee16c8ea5765ea8a023d
jong2 <ehdclr@gmail.com>
2022-11-12 16:21:25 +0900

    :package: Chore : UPDATE Mysql config.js 수정
    MYSQL config.js 수정

---
+++ sever/config/config.js
@@ -0,0 +1,39 @@
+const development = {
+  username: "root",
+  password: null,
+  database: "database_development",
+  host: "127.0.0.1",
+  dialect: "mysql",
+  timezone: "+09:00",
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
+const test = {
+  username: "root",
+  password: null,
+  database: "database_test",
+  host: "127.0.0.1",
+  dialect: "mysql",
+  timezone: "+09:00",
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
+const production = {
+  username: "root",
+  password: null,
+  database: "database_production",
+  host: "127.0.0.1",
+  dialect: "mysql",
+  timezone: "+09:00",
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
