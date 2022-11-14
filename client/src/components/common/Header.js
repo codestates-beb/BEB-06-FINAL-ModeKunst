de9e493a341af588b5a63e515a84a5290572fdaf
parixscm <parixscm@gmail.com>
2022-11-15 02:25:29 +0900

    🚑 Fix: UPDATE 헤더 컴포넌트 search 수정

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -55,8 +55,8 @@ export default function Header() {
 
       {isUser ? (
         <div className="self-end place-self-end grid grid-cols-4 gap-16">
-          <div className="p-1 rounded-full hover:bg-lime-100">
-            <Link to="/write">
+          <div className="p-1 rounded-full hover:bg-lime-100 select-none flex justify-center items-center">
+            <button>
               <svg
                 fill="none"
                 viewBox="0 0 24 24"
@@ -70,7 +70,7 @@ export default function Header() {
                   d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
                 />
               </svg>
-            </Link>
+            </button>
           </div>
           <div className="p-1 rounded-full hover:bg-yellow-100">
             <Link to="/write">
@@ -101,7 +101,7 @@ export default function Header() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                 />
               </svg>
             </Link>
