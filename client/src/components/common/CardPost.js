183730bee6c27af89a6ed73cc96c695d68efaf57
jangsam <gudwns0827@gmail.com>
2022-11-22 02:04:26 +0900

    :memo: Feat: Add mypage

--- client/src/components/common/CardPost.js
+++ client/src/components/common/CardPost.js
@@ -1,26 +1,28 @@
+import {Link} from "react-router-dom";
+
 export default function CardPost({ item, hotpost }) {
+  console.log(hotpost);
   return (
     <div
       key={item}
       className="relative w-full transition-all duration-75 ease-in hover:scale-105 cursor-pointer"
     >
-      <img
-        alt="hotpost_image"
-        src={hotpost}
-        className="object-contain rounded-lg shadow-xl block"
-      />
+      <Link to={`/post/${hotpost.id}`}>
+        <img
+            alt="hotpost_image"
+            src={hotpost.image_1}
+            className="object-contain rounded-lg shadow-xl block"
+        />
+      </Link>
       <div className="py-1 absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 rounded-b-lg">
         <div className="px-2 flex items-center font-medium text-slate-200 space-x-2">
-          <div className="w-6 h-6 bg-blue-500 rounded-full">
-            {/* 👇🏻 밑에 img 태그 주석 해제 후, src 태그에 전달받은 이미지 넣어줘야 함 */}
-            {/* <img alt="profile_image" /> */}
-          </div>
-          <span className="text-sm">Sample Title</span>
+
+          <span className="text-sm">{hotpost.title}</span>
           <div className="text-xs flex flex-col">
-            <span>5시간 전</span>
+            <span>{hotpost.createdAt} </span>
             <div className="space-x-2">
               <span>likes</span>
-              <span>views</span>
+              <span>views: {hotpost.views}</span>
             </div>
           </div>
         </div>
--- sever/controllers/users/mypage.js
+++ sever/controllers/users/mypage.js
@@ -1,4 +1,4 @@
-const { User, Follow, Post, Token } = require("../../models");
+const { User, Follow, Post, Token, Like } = require("../../models");
 const { literal  } = require('sequelize');
 const { many } = require('../function/createdAt');
 
@@ -7,6 +7,7 @@ module.exports = {
   get: async (req, res) => {
     //TODO 1. 유저 정보 가져오기
     //TODO 2. 팔로워 숫자 팔로잉 한 정보들 가져오기
+    const login_user = req.session.user?.nickname;
     const nickname = req.params.nickname;
     console.log(`입력 받은 nickname: ${nickname}`);
     try {
@@ -16,6 +17,16 @@ module.exports = {
         attributes: ['nickname', 'profile_img', 'height', 'weight', 'gender', 'sns_url'],
         raw: true
       });
+
+      let isFollow = await Follow.findOne({
+        where: { follower: login_user, following: nickname}
+      });
+
+      isFollow = !!isFollow;
+
+      const follow_amount = await Follow.count({
+        where: { following: nickname }
+      })
 
       const posts = await Post.findAll({
         where: {UserNickname: nickname},
@@ -29,20 +40,42 @@ module.exports = {
         return new Date(post.createdAt);
       });
 
-      const diff = many(dateFormatPosts);
-      console.log(diff)
+      let diff = many(dateFormatPosts);
+
       posts.map((post, i) => {
         post.createdAt = diff[i]
       });
 
+      const likePosts = await Post.findAll({
+        include: [ { model: Like, where: { UserNickname: nickname }, attributes: ['PostId'] } ],
+        attributes: ['id', 'image_1', 'title', 'content', 'category', 'views', 'createdAt', 'UserNickname'],
+        raw: true
+      });
+
+      const dateFormatLikePosts = likePosts.map((post) =>{
+        return new Date(post.createdAt);
+      });
+
+      diff = many(dateFormatLikePosts);
+
+      likePosts.map((post, i) => {
+        post.createdAt = diff[i]
+      })
+
       if(user){
         try{
-          const followers = await Follow.findAll({
+          let followers = await Follow.findAll({
             where: { following: req.params.nickname },
             include: { model: User, attributes:['profile_img'], as: 'Follower'},
             attributes: ['Follower'],
             raw: true
           });
+          followers = followers.map((a) => {
+            let data = { profile_img : a['Follower.profile_img']}
+            delete (a['Follower.profile_img']);
+            let format =  {...a, ...data}
+            return(format)
+          })
 
           const realFollowers = [];
           for(const follower of followers){
@@ -53,12 +86,19 @@ module.exports = {
             }
           }
 
-          const followings = await Follow.findAll({
+          let followings = await Follow.findAll({
             where: { follower: req.params.nickname },
             include: { model: User, attributes:['profile_img'], as: 'Following'},
             attributes: ['Following'],
             raw: true
           });
+
+          followings = followings.map((a) => {
+            let data = { profile_img : a['Following.profile_img']}
+            delete (a['Following.profile_img']);
+            let format =  {...a, ...data}
+            return(format)
+          })
 
           const realFollowings = [];
           for(const following of followings){
@@ -68,16 +108,18 @@ module.exports = {
               realFollowings.push(data);
             }
           }
-          console.log(realFollowings)
 
 
           return res.status(200).json({
             message: `${nickname}님의 마이페이지`,
             data: {
                 user,
+                follow_amount,
                 posts,
+                likePosts,
                 followers: realFollowers,
                 followings: realFollowings,
+                isFollow
             }
           });
         } catch (e) {
