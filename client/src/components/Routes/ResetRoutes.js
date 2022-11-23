aee443d5f270385fe69a9e2daa6106146505bfc6
parixscm <parixscm@gmail.com>
2022-11-23 12:59:46 +0900

    :memo: Feat: ADD 유저페이지 nft 섹션 제외

--- client/src/components/Routes/ResetRoutes.js
+++ client/src/components/Routes/ResetRoutes.js
@@ -3,12 +3,9 @@ import { ResetMyInfo, ResetPassword } from "../../pages";
 
 export default function ResetRoutes() {
   return (
-    <>
-      <h1>내 정보 수정</h1>
-      <Routes>
-        <Route path="password" element={<ResetPassword />} />
-        <Route path="myinfo" element={<ResetMyInfo />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="password" element={<ResetPassword />} />
+      <Route path="myinfo" element={<ResetMyInfo />} />
+    </Routes>
   );
 }
