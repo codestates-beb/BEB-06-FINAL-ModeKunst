08a6a4cc520f8c2debc79f43845691ea4097ec86
parixscm <parixscm@gmail.com>
2022-11-24 15:07:41 +0900

    :memo: Feat: ADD 게시글보기 페이지 리뷰

--- sever/controllers/posts/review.js
+++ sever/controllers/posts/review.js
@@ -1,237 +1,254 @@
-const {review} = require("./index");
-const {many} = require('../function/createdAt');
-const { Review, User, Server, Post, Token_price} = require('../../models');
-const {web3, abi20, serverPKey, getBalance} = require("../../contract/Web3");
+const { review } = require("./index");
+const { many } = require("../function/createdAt");
+const { Review, User, Server, Post, Token_price } = require("../../models");
+const { web3, abi20, serverPKey, getBalance } = require("../../contract/Web3");
 
 module.exports = {
+  // 리뷰 작성 2 토큰
+  post: async (req, res) => {
+    const userInfo = req.session.user;
+    const nickname = userInfo.nickname;
+    const userAddress = userInfo.address;
+    const { postId } = req.params;
+    const { content } = req.body;
+    console.log(
+      `입력받은 nickname: ${nickname}, postId: ${postId}, content: ${content}`
+    );
+    try {
+      const review = await Review.findOne({
+        where: { UserNickname: nickname, PostId: postId },
+        paranoid: false,
+      });
 
-    // 리뷰 작성 2 토큰
-    post: async (req, res) => {
-        const userInfo = req.session.user;
-        const nickname = userInfo.nickname;
-        const userAddress = userInfo.address;
-        const { postId } = req.params;
-        const { content } = req.body;
-        console.log(`입력받은 nickname: ${nickname}, postId: ${postId}, content: ${content}`);
+      if (!review) {
+        // 여기가 새 리뷰
         try {
-            const review = await Review.findOne({
-                where: { UserNickname: nickname, PostId: postId},
-                paranoid: false
-            });
+          const price = await Token_price.findOne({
+            where: { id: 1 },
+            attributes: ["review"],
+            raw: true,
+          });
+          const review_price = price.review;
 
-            if(!review){
-                // 여기가 새 리뷰
-                try{
-                    const price = await Token_price.findOne({ where: { id: 1 }, attributes: ['review'], raw: true });
-                    const review_price = price.review;
+          const serverInfo = await Server.findOne({
+            attributes: ["address", "erc20"],
+            raw: true,
+          });
+          const { address, erc20 } = serverInfo;
 
-                    const serverInfo = await Server.findOne({
-                        attributes: ['address', 'erc20'],
-                        raw: true
-                    });
-                    const { address, erc20 } = serverInfo;
+          const contract = await new web3.eth.Contract(abi20, erc20);
 
-                    const contract = await new web3.eth.Contract(abi20, erc20);
+          const data = contract.methods
+            .transfer(userAddress, review_price)
+            .encodeABI();
+          const rawTransaction = { to: erc20, gas: 100000, data: data };
+          const signTx = await web3.eth.accounts.signTransaction(
+            rawTransaction,
+            serverPKey
+          );
+          await web3.eth.sendSignedTransaction(signTx.rawTransaction);
 
-                    const data = contract.methods.transfer(userAddress, review_price).encodeABI();
-                    const rawTransaction = { 'to': erc20, 'gas': 100000, 'data': data };
-                    const signTx = await web3.eth.accounts.signTransaction(rawTransaction, serverPKey);
-                    await web3.eth.sendSignedTransaction(signTx.rawTransaction);
+          const server_eth = await getBalance(address);
+          //트랜잭션 보내고 서버 주소에서 남은 이더
 
-                    const server_eth = await getBalance(address);
-                    //트랜잭션 보내고 서버 주소에서 남은 이더
+          const serverBalance = await contract.methods
+            .balanceOf(address)
+            .call();
+          //서버가 보유중인 토큰 양
 
-                    const serverBalance = await contract.methods.balanceOf(address ).call();
-                    //서버가 보유중인 토큰 양
+          const clientBalance = await contract.methods
+            .balanceOf(userAddress)
+            .call();
+          //유저가 보유 중인 토큰 양
 
-                    const clientBalance = await contract.methods.balanceOf(userAddress).call();
-                    //유저가 보유 중인 토큰 양
+          await Server.update(
+            { eth_amount: server_eth, token_amount: serverBalance },
+            { where: { address: address } }
+          );
 
-                    await Server.update({ eth_amount: server_eth, token_amount: serverBalance }, { where: { address: address } });
+          await User.update(
+            { token_amount: clientBalance },
+            { where: { nickname: nickname } }
+          );
 
-                    await User.update({ token_amount: clientBalance }, { where: { nickname: nickname } });
+          await Review.create({
+            content: content,
+            UserNickname: nickname,
+            PostId: postId,
+          });
 
+          const review_counts = await Review.count({
+            where: { PostId: postId },
+          });
 
-                    await Review.create(
-                        { content: content, UserNickname: nickname, PostId: postId },
-                    );
+          await Post.update(
+            { reviews_num: review_counts },
+            { where: { id: postId } }
+          );
 
-                    const review_counts = await Review.count({
-                        where: { PostId: postId }
-                    });
+          const reviews = await Review.findAll({
+            attributes: ["id", "content", "createdAt", "UserNickname"],
+            where: { PostId: postId },
+            raw: true,
+          });
 
-                    await Post.update({ reviews_num: review_counts }, {where: { id: postId } })
-
-                    const reviews = await Review.findAll({
-                        attributes: ['id', 'content', 'createdAt', 'UserNickname'],
-                        where: { PostId: postId },
-                        raw: true,
-                    });
-
-                    res.status(200).json({
-                        message: '리뷰가 작성되었습니다.',
-                        data: {
-                            review_counts: review_counts,
-                            reviews: reviews
-                        }
-                    });
-                } catch (e) {
-                    console.log(`sequelize Err`);
-                    console.log(e);
-                }
-            }else{
-                //리뷰 삭제 후 다시 작성
-                if(review?.dataValues.deletedAt){
-                    await Review.destroy({
-                        where: { UserNickname: nickname, PostId: postId},
-                        force: true
-                    });
-
-                    await Review.create(
-                        { content: content, UserNickname: nickname, PostId: postId },
-                    );
-
-                    const review_counts = await Review.count({
-                        where: { PostId: postId }
-                    });
-
-                    const reviews = await Review.findAll({
-                        attributes: ['id', 'content', 'createdAt', 'UserNickname'],
-                        where: { PostId: postId },
-                        raw: true
-                    });
-
-
-                    res.status(200).json({
-                        message: '리뷰가 작성 되었습니다.',
-                        data: {
-                            review_counts: review_counts,
-                            reviews: reviews
-                        }
-                    })
-                }else{
-                    res.status(401).json({
-                        message: '이미 리뷰를 작성했습니다.'
-                    })
-                }
-            }
+          res.status(200).json({
+            message: "리뷰가 작성되었습니다.",
+            data: {
+              review_counts: review_counts,
+              reviews: reviews,
+            },
+          });
         } catch (e) {
-            console.log(`sequelize Err`);
-            console.log(e)
+          console.log(`sequelize Err`);
+          console.log(e);
         }
+      } else {
+        //리뷰 삭제 후 다시 작성
+        if (review?.dataValues.deletedAt) {
+          await Review.destroy({
+            where: { UserNickname: nickname, PostId: postId },
+            force: true,
+          });
 
-    },
+          await Review.create({
+            content: content,
+            UserNickname: nickname,
+            PostId: postId,
+          });
 
-    get: async (req,res) => {
-        //전체 리뷰 가져오기 findAll
+          const review_counts = await Review.count({
+            where: { PostId: postId },
+          });
 
-        let {postId} = req.params;
-        let pageNum = req.query.page; //무한스크롤 페이지네이션
-        let offset = 0;//초기 오프셋
+          const reviews = await Review.findAll({
+            attributes: ["id", "content", "createdAt", "UserNickname"],
+            where: { PostId: postId },
+            raw: true,
+          });
 
-        if(pageNum >1){
-            offset = 8 * (pageNum -1);
+          res.status(200).json({
+            message: "리뷰가 작성 되었습니다.",
+            data: {
+              review_counts: review_counts,
+              reviews: reviews,
+            },
+          });
+        } else {
+          res.status(401).json({
+            message: "이미 리뷰를 작성했습니다.",
+          });
         }
-
-
-        const reviewList = await Review.findAll({
-            offset: offset,
-            limit: 8,
-            order:[['createdAt','DESC']],
-            where:{ Postid : postId }
-        });
-
-        console.log('리뷰 목록',reviewList);
-
-        const dateFormatReviews= reviewList.map((review)=>{
-            return new Date(review.createdAt);
-        });
-
-        let diff = many(dateFormatReviews);
-
-        const reviews = reviewList.map((el,idx)=>{
-            return {
-                id: el.id,
-                nickname : el.UserNickname,
-                content: el.content,
-                create_at: diff[idx],
-            }
-        })
-
-        return res.status(200).json({reviews});
-
-
-    },
-
-    // 리뷰 수정
-    put: async (req, res) => {
-        const nickname = req.session.user?.nickname;
-        const { postId } = req.params;
-        const { content } = req.body;
-
-        try {
-            await Review.update(
-                { content: content},
-                { where: { UserNickname: nickname, PostId: postId}}
-            );
-
-            const reviews_counts = await Review.count({
-                where: { PostId: postId }
-            });
-
-            const reviews = await Review.findAll({
-                attributes: ['id', 'content', 'createdAt', 'UserNickname'],
-                where: { PostId: postId },
-                raw: true
-            });
-
-            res.status(200).json({
-                message: '리뷰가 수정되었습니다.',
-                data: {
-                    reviews_counts: reviews_counts,
-                    reviews: reviews
-                }
-            })
-        } catch (e) {
-            console.log(`sequelize Err`);
-            console.log(e)
-        }
-    },
-
-    // 리뷰 삭제
-    delete: async (req, res) => {
-        const nickname = req.session.user?.nickname;
-        const { postId } = req.params;
-
-
-        try {
-            await Review.destroy({
-                where: { UserNickname: nickname, PostId: postId }
-            });
-
-            const review_counts = await Review.count({
-                where: { PostId: postId }
-            });
-
-            const reviews = await Review.findAll({
-                attributes: ['id', 'content', 'createdAt', 'UserNickname'],
-                where: { PostId: postId },
-                raw: true
-            });
-
-            res.status(200).json({
-                message: '리뷰가 삭제되었습니다.',
-                data: {
-                    review_counts: review_counts,
-                    reviews: reviews
-                }
-            })
-
-        } catch (e) {
-            console.log(`sequelize Err`);
-            console.log(e)
-        }
-
+      }
+    } catch (e) {
+      console.log(`sequelize Err`);
+      console.log(e);
     }
-}
+  },
 
+  get: async (req, res) => {
+    //전체 리뷰 가져오기 findAll
+
+    let { postId } = req.params;
+    let pageNum = req.query.page; //무한스크롤 페이지네이션
+    let offset = 0; //초기 오프셋
+
+    if (pageNum > 1) {
+      offset = 4 * (pageNum - 1);
+    }
+
+    const reviewList = await Review.findAll({
+      offset: offset,
+      limit: 4,
+      order: [["createdAt", "DESC"]],
+      where: { Postid: postId },
+    });
+
+    console.log("리뷰 목록", reviewList);
+
+    const dateFormatReviews = reviewList.map(review => {
+      return new Date(review.createdAt);
+    });
+
+    let diff = many(dateFormatReviews);
+
+    const reviews = reviewList.map((el, idx) => {
+      return {
+        id: el.id,
+        nickname: el.UserNickname,
+        content: el.content,
+        create_at: diff[idx],
+      };
+    });
+
+    return res.status(200).json({ reviews });
+  },
+
+  // 리뷰 수정
+  put: async (req, res) => {
+    const nickname = req.session.user?.nickname;
+    const { postId } = req.params;
+    const { content } = req.body;
+
+    try {
+      await Review.update(
+        { content: content },
+        { where: { UserNickname: nickname, PostId: postId } }
+      );
+
+      const reviews_counts = await Review.count({
+        where: { PostId: postId },
+      });
+
+      const reviews = await Review.findAll({
+        attributes: ["id", "content", "createdAt", "UserNickname"],
+        where: { PostId: postId },
+        raw: true,
+      });
+
+      res.status(200).json({
+        message: "리뷰가 수정되었습니다.",
+        data: {
+          reviews_counts: reviews_counts,
+          reviews: reviews,
+        },
+      });
+    } catch (e) {
+      console.log(`sequelize Err`);
+      console.log(e);
+    }
+  },
+
+  // 리뷰 삭제
+  delete: async (req, res) => {
+    const nickname = req.session.user?.nickname;
+    const { postId } = req.params;
+
+    try {
+      await Review.destroy({
+        where: { UserNickname: nickname, PostId: postId },
+      });
+
+      const review_counts = await Review.count({
+        where: { PostId: postId },
+      });
+
+      const reviews = await Review.findAll({
+        attributes: ["id", "content", "createdAt", "UserNickname"],
+        where: { PostId: postId },
+        raw: true,
+      });
+
+      res.status(200).json({
+        message: "리뷰가 삭제되었습니다.",
+        data: {
+          review_counts: review_counts,
+          reviews: reviews,
+        },
+      });
+    } catch (e) {
+      console.log(`sequelize Err`);
+      console.log(e);
+    }
+  },
+};
