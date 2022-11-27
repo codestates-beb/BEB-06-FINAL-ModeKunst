2cd17c6190e1d85b204634faac39bef824ef14c2
jinlimi <eptjs56@gmail.com>
2022-11-27 23:39:59 +0900

    :lipstick: Change: css 설정 변경(폰트, 배경색)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["NanumSquareNeo-Variable", "sans-serif"],
+        content: ["Pretendard-Regular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
