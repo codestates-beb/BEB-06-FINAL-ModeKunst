7364115112a7208606969482753b20633917fd13
jinlimi <eptjs56@gmail.com>
2022-11-29 00:58:11 +0900

    :memo: Feat: ADD 푸터 추가

---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import logo from "../../assets/modekunst_logo_3.png";
+
+function Footer() {
+  const adminInfo = useSelector(state => state.admin);
+  const isAdmin = adminInfo.isAdmin;
+  const adminNickname = adminInfo.nickname;
+
+  return (
+    <div className="bottom-0 w-full h-56 bg-black">
+      <img src={logo} className="w-48 py-4 px-2" alt="logo" />
+      <div className="text-white ml-2 font-content">
+        Copyright © Modekunst. All rights reserved
+      </div>
+      {!isAdmin ? (
+        <Link to="/adminlogin">
+          <div className="text-white mt-2 ml-2 font-content">ADMIN PAGE</div>
+        </Link>
+      ) : (
+        <Link to={"/admin"}>
+          <div className="text-white mt-2 ml-2 font-content">ADMIN PAGE</div>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default Footer;
