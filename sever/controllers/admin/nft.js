af38c1cf0e52fd50db1c72128551fa05c1f6e232
jinlimi <eptjs56@gmail.com>
2022-12-02 16:02:45 +0900

    :bug: Fix: git pull

--- sever/controllers/admin/nft.js
+++ sever/controllers/admin/nft.js
@@ -1,73 +1,65 @@
-const {User,Server,Token } =require('../../models');
-const { web3,abi721} = require('../../contract/Web3');
+const { User, Server, Token } = require("../../models");
+const { web3, abi721 } = require("../../contract/Web3");
 
+module.exports = {
+  nftmint: async (req, res) => {
+    const { host } = req.headers;
 
+    //상위 10명 (팔로워수 많은 사람)
 
-module.exports={
-    nftmint: async(req,res) =>{
+    const { title, metadataUrl, description } = req.body;
 
-        console.log(req.file);
-        const {host} = req.headers;
+    console.log(title);
+    console.log(metadataUrl);
+    console.log(description);
 
-        //상위 10명 (팔로워수 많은 사람)
+    const contractServer = await Server.findOne({
+      attributes: ["erc721", "address"],
+      raw: true,
+    });
+    const celebrity = await User.findAll({
+      limit: 10,
+      order: [["followers_num", "DESC"]],
+      attributes: ["nickname"],
+    });
 
+    const celebList = [];
+    celebrity.map(el => {
+      celebList.push(el.dataValues.nickname);
+    });
 
-        const {title, description} = req.body;
+    const celebData = {
+      celeb: celebList,
+    };
 
-        const contractServer = await Server.findOne({
-            attributes:["erc721","address"],
-            raw:true
-        });
-        const celebrity = await User.findAll({
-            limit:10,
-            order:[["followers_num","DESC"]],
-            attributes:["nickname"]
-        });
+    const { address, erc721 } = contractServer;
 
+    const contract721 = await new web3.eth.Contract(abi721, erc721);
 
+    //민팅할 때 , 상위 10 명 메타데이터에 담아서 민팅
+    //메타 데이터에 민팅 하면서 넣기
+    const mintNft = await contract721.methods
+      .mintNFT(address, JSON.stringify(celebData))
+      .send({ from: address, gas: 1321250 });
+    const mint_amount = await contract721.methods.totalSupply().call();
+    const tokenUrI = await contract721.methods.tokenURI(mint_amount).call();
+    console.log(tokenUrI, "엌엌");
 
-        const celebList = [];
-        celebrity.map((el)=>{
-            celebList.push(el.dataValues.nickname);
-        })
+    //nft 상위 10명은
+    celebList.map(async el => {
+      await Token.create({
+        title: title,
+        description: description,
+        tx_hash: mintNft.transactionHash,
+        token_url: metadataUrl,
+        UserNickname: el,
+        image: `http://${host}/${req.file.path}`,
+      });
+    });
 
-        const celebData = {
-            celeb: celebList
-        }
-
-        const {address, erc721} = contractServer;
-
-
-
-        const contract721 = await new web3.eth.Contract(abi721,erc721);
-
-        //민팅할 때 , 상위 10 명 메타데이터에 담아서 민팅
-        //메타 데이터에 민팅 하면서 넣기
-        const mintNft = await contract721.methods.mintNFT(address,JSON.stringify(celebData)).send({from:address, gas:1321250});
-        const mint_amount = await contract721.methods.totalSupply().call();
-        const tokenUrI = await contract721.methods.tokenURI(mint_amount).call();
-        console.log(tokenUrI,'엌엌');
-
-        //nft 상위 10명은
-        celebList.map(async (el)=>{
-            await Token.create({
-                title:title,
-                description:description,
-                tx_hash : mintNft.transactionHash,
-                token_url: data.url,
-                UserNickname:el,
-                image: `http://${host}/${req.file.path}`,
-
-            });
-        })
-
-
-        return res.status(200).json({
-            message:`${title} 시즌 토큰 발급을 완료 했습니다.`,
-            nft_amount: mint_amount});
-
-
-    },
-
-
-}
+    return res.status(200).json({
+      message: `${title} 시즌 토큰 발급을 완료 했습니다.`,
+      nft_amount: mint_amount,
+    });
+  },
+};
