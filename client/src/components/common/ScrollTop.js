b1f33518e77413ec0515e2d1be7c6bb7653cee6f
parixscm <parixscm@gmail.com>
2022-12-03 16:30:24 +0900

    :bug: Fix: FIX 마이페이지 상단으로 스크롤 버그 수정

--- client/src/components/common/ScrollTop.js
+++ client/src/components/common/ScrollTop.js
@@ -3,7 +3,11 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollTop() {
   const { pathname } = useLocation();
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // 마이페이지에서 nested routes 링크 이동하면 불필요하게 위로 스크롤 되는 현상 방지
+    if (!pathname.includes("user")) {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
 }
